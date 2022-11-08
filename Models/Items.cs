cdd9131a9d64521d045d1544434a87c684224c8b
Jake Erven <jakeerven@gmail.com>
2022-11-08 10:28:57 -0600

    Added Models and Some Model Info

---
+++ Models/Items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class Items
+    {
+        private int ItemID;
+        private string ItemName;
+        private double Price;
+
+
+
+
+
+        public Items() { }
+    }
+}
---
+++ Models/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class Orders
+    {
+        public int OrderID;
+        public int CustID;
+        public string CustAddress;
+
+
+
+        public Orders() { }
+    }
+}

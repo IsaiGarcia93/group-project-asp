b9d8ca498d18a4964ec268841c4ee17b0b3a0695
Isai Gonzalez Garcia <ig705295@southeast.edu>
2022-11-08 15:46:44 -0600

    Item Controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using GroupProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly UserDBContext _context;
+
+        public ItemsController(UserDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Item
+        public async Task<IActionResult> Index()
+        {
+            var userDbContext = _context.ItemsModel.
+            return View();
+        }
+    }
+}

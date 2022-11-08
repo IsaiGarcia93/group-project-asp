e2fa52703c7efbd09230693cfe015ab62cec14b1
Jake Erven <jakeerven@gmail.com>
2022-11-08 12:13:22 -0600

    Reconfigure Register and Login Page to work correctly

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GroupProject.Areas.Identity.Data;
+using GroupProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GroupProject.Areas.Identity.IdentityHostingStartup))]
+namespace GroupProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserDBContextConnection")));
+
+                services.AddDefaultIdentity<AppliocationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserDBContext>();
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,8 @@ namespace GroupProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +46,7 @@ namespace GroupProject
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +54,7 @@ namespace GroupProject
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
